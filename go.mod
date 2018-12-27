b672d3fe2ba7b82bb3e8264dea127c484fcf821e
zhaixiaolei <zhaixiaolei@bytedance.com>
2018-12-27 09:14:40 +0800

    摸底 生成器方法

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/totoleo/tero
+
+require (
+	github.com/philhofer/fwd v1.0.0 // indirect
+	github.com/tinylib/msgp v1.1.0
+	github.com/ttacon/chalk v0.0.0-20160626202418-22c06c80ed31
+	golang.org/x/tools v0.0.0-20181221235234-d00ac6d27372
+)
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+/*
+Kitc client 生成改进方案
+必要的输入：
+1. Idl 文件目录，默认位置：$HOME/idl/
+2. 被调用方的 psm
+3. 调用方的 psm , 默认是 clients/../scripts/setting.py 内的配置
+4. 服务的别名
+5. 生成文件的名称，默认是: {psm}_client.go {psm}_client_test.go
+6. 输出目录：clients/
+7.
+*/
+func main() {
+	file := os.Getenv("GOFILE")
+
+	fileSet, err := parse.File(file, true)
+	fmt.Println(fileSet, err)
+	if err != nil {
+		panic(err)
+	}
+	for k, set := range fileSet.Specs {
+		fmt.Println(k, set.Pos(), set.End())
+	}
+
+}
